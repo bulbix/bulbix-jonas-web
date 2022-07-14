a7ab04084252ae79f32a636056ab55745f489aeb
Fernando Prado <out_lfernandop@apoyoeconomico.com.mx>
2022-07-14 00:12:59 -0500

    feat: config heroku

--- services/bookService.js
+++ services/bookService.js
@@ -1,20 +1,24 @@
 import axios from 'axios'
 
+const baseURL = 'https://safe-hamlet-62856.herokuapp.com/'
+//const baseURL = 'http://127.0.0.1:8000/'
+
+
 const service = (url) => axios.create({
     baseURL: url,
     timeout: 15000,
 });
 
 export function addBook(body) {
-    return service('http://127.0.0.1:8000/').post('add_book', body);
+    return service(baseURL).post('add_book', body);
 }
 
 export function searchBook(query) {
-    return service('http://127.0.0.1:8000/').get('search_book?q=' + query);
+    return service(baseURL).get('search_book?q=' + query);
 }
 
 export function consultIsbn(isbn) {
-    return service('http://127.0.0.1:8000/').get('consult_isbn?isbn=' + isbn);
+    return service(baseURL).get('consult_isbn?isbn=' + isbn);
 }
 
 
