c4b3a673ff7328c77a752ae53de7347fd915fe21
Fernando Prado <out_lfernandop@apoyoeconomico.com.mx>
2022-07-14 20:40:06 -0500

    fix: miss baseurl

--- services/bookService.js
+++ services/bookService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-//const baseURL = 'https://safe-hamlet-62856.herokuapp.com/'
-const baseURL = 'http://127.0.0.1:8000/'
+const baseURL = 'https://safe-hamlet-62856.herokuapp.com/'
+//const baseURL = 'http://127.0.0.1:8000/'
 
 
 const service = (url) => axios.create({
